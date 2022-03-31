2ce34ded8afecc27108a833fed97b503d74ff059
Balaji-Gopalakrishnan <74197288+Balaji-Gopalakrishnan@users.noreply.github.com>
2022-03-31 12:21:42 +0530

    Add files via upload

---
+++ getkey.c
@@ -0,0 +1,15 @@
+#include"calendar.h"
+
+int getkey()
+{
+    int ch;
+    ch=getch();
+     if(ch==0)
+    {
+        printf("zero");
+        ch=getch();
+
+        return ch;
+    }
+    return ch;
+}
