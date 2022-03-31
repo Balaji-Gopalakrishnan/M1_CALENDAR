e4f68e18db6e840c7120675a05c6c3e103129c45
Balaji-Gopalakrishnan <74197288+Balaji-Gopalakrishnan@users.noreply.github.com>
2022-03-31 10:23:15 +0530

    Add files via upload

---
+++ 2_Implementations/inc/calendar.h
@@ -0,0 +1,12 @@
+#ifndef CALENDAR_H_INCLUDED
+#define CALENDAR_H_INCLUDED
+
+#include<stdio.h>
+#include<conio.h>
+#include<windows.h>
+
+int getkey();
+void display(int,int,int,int[]);
+void calendar(int,int);
+
+#endif // CALENDAR_H_INCLUDED
